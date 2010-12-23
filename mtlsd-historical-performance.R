0f1d9e849ca5b4878eb8f3ed7934b266de3fd885
Tom Moertel <tom@moertel.com>
2010-12-23 13:43:30 -0500

    Separated data-prep from main analysis.  Used Google Refine to clean data.

--- mtlsd-historical-performance.R
+++ mtlsd-historical-performance.R
@@ -55,83 +55,9 @@ school_districts_of_interest <- local({
 subject_districts <- subset(school_districts_of_interest, !is.na(district))
 
 
+## Load the data and prepare it to be plotted
 
-##=============================================================================
-## Prepare the data for analysis
-##
-## Here I load the data sets and prepare them for my analysis.  I use
-## 2002-2010 PSSA data from the PA Dept. of Education.
-##=============================================================================
-
-## First, I read in the data sets
-
-load_pssa <- function(name,
-                      year,
-                      math,
-                      reading,
-                      skip = 0,
-                      selector = function(df) df,
-                      aun = 1,
-                      county = 2,
-                      district = 4,
-                      grade = 5) {
-  df <- read.csv(paste(sep="", "data/", name),
-                 skip = skip, na.strings = "#NULL!",
-                 as.is=c(grade, math, reading))
-  df <- subset(df, ! grepl("total", df[[grade]], ignore.case=T))
-  df <- selector(df)
-  df <- data.frame(year = year,
-                   aun = df[[aun]],
-                   county = df[[county]],
-                   district = df[[district]],
-                   grade = as.numeric(df[[grade]]),
-                   math = as.numeric(df[[math]]),
-                   reading = as.numeric(df[[reading]]))
-}
-
-pssa_2002 <- load_pssa("2002MathandReadingperformancelevelsalldistricts.csv",
-                       2002, math=8, reading=12)
-
-pssa_2003 <- load_pssa("2003MathandReadingperformancelevelsalldistricts.csv",
-                       2003, math=8, reading=12)
-
-pssa_2004 <- load_pssa("2004MathandReadingperformancelevelsalldistricts.csv",
-                       2004, math=8, reading=12)
-
-pssa_2005 <- load_pssa("2005MathandReadingperformancelevelsalldistricts.csv",
-                       2005, math=9, reading=14)
-
-pssa_2006 <- load_pssa("2006 District Level PSSA Results.csv",
-                       2006, math=8, reading=14, skip=3)
-
-pssa_2007 <- load_pssa("2007 District Level PSSA Results.csv",
-                       2007, math=7, reading=12, skip=4)
-
-pssa_2008 <- load_pssa("2008 PostAYP District Level PSSA Results.csv",
-                       2008, math=7, reading=12, skip=4)
-
-pssa_2009 <- load_pssa("PSSA_Results_Math_and_Reading_District_2009.csv",
-                       2009, math=8, reading=13, skip=3,
-                       selector = function(df) {
-                         subset(df, Group=="All Students")
-                       })
-
-pssa_2010 <- load_pssa("PSSA_Results_Math_and_Reading_District_2010.csv",
-                       2010, math=8, reading=13, skip=2,
-                       selector = function(df) {
-                         subset(df, Group=="All Students")
-                       })
-
-
-pssa_merged <- rbind(pssa_2002,
-                     pssa_2003,
-                     pssa_2004,
-                     pssa_2005,
-                     pssa_2006,
-                     pssa_2007,
-                     pssa_2008,
-                     pssa_2009,
-                     pssa_2010)
+pssa_merged <- read.csv("data/pssa-merged-and-cleaned.csv")
 
 pssa_merged_extended <- local({
   df <- merge(pssa_merged, school_districts_of_interest, all.x=T)
@@ -145,7 +71,6 @@ pssa_merged_extended <- local({
   })
 })
 
-
 pssa_melted <- melt(pssa_merged_extended, measure.vars = c("math", "reading"))
 
 pssa_melted <- subset(pssa_melted, !is.na(value))
@@ -154,6 +79,7 @@ pssa_ecdf <- ddply(pssa_melted, .(year, grade, variable),
                    transform,
                    value_ecdf = ecdf(value)(value))
 
+## Plot the data
 
 p <-
 qplot(year, value_ecdf,
